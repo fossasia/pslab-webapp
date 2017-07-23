effe43702f1a899fa3e8e8495969952126af1218
jithinbp <jithinbp@gmail.com>
2017-07-23 21:16:37 +0530

    bug fix. missing APP_SETTINGS global

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object(os.environ['APP_SETTINGS'])
 print (os.environ['DATABASE_URL'])
 
 
