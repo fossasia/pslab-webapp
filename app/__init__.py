f874f4a4e50903875137f43af04ab20741be5cdd
jithinbp <jithinbp@gmail.com>
2017-07-23 21:10:11 +0530

    missing parenthesis bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
-print os.environ['DATABASE_URL']
+print (os.environ['DATABASE_URL'])
 
 
 # for sqlite ,DATABASE_URL = 'sqlite:////tmp/test.db'
@@ -16,26 +16,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 
-
 from app import views
-'''
-from flask.ext.mysql import MySQL
 
-from werkzeug.contrib.fixers import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app)
-
-app.secret_key = 'yet another secret key'
-
-mysql = MySQL()
-
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root123'
-app.config['MYSQL_DATABASE_DB'] = 'VirtualUsers'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
-
-conn = mysql.connect()
-cursor = conn.cursor()
-
-'''
