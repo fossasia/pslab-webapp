194c738599175beba2e29e59c34c21fc0379555e
jithinbp <jithinbp@gmail.com>
2017-07-27 01:06:23 +0530

    added backend routine to write user scripts to the database
     fixes #16 . scripts go into a new table along with the user-id for searchability and access control

--- app/db_handler.py
+++ app/db_handler.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -14,6 +15,27 @@ class User(db.Model):
 
 	def __repr__(self):
 		return '<User %r>' % self.username
+
+
+class UserCode(db.Model):
+	__tablename__ = 'userCode'
+	id = db.Column(db.Integer, primary_key=True)
+	user = db.Column(db.String(80), unique=True)
+	title = db.Column(db.String(100), unique=True)
+	code = db.Column(db.Text)
+	pub_date = db.Column(db.DateTime)
+
+	def __init__(self, user, title,code,pub_date=None):
+		self.user = user
+		self.title = title
+		self.code = code
+		if pub_date is None:
+		    pub_date = datetime.utcnow()
+		self.pub_date = pub_date
+
+	def __repr__(self):
+		return '<UserCode %r>' % self.email
+
 
 
 db.create_all()
