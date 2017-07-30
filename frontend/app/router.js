b968b03ba8591bb30ab34ed810484806ce13592f
jithinbp <jithinbp@gmail.com>
2017-07-30 11:38:56 +0530

    adding boilerplate code for fully decoupled frontend based on EmberJS
    WIP issue #21 .

---
+++ frontend/app/router.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('signIn');
+  this.route('signOut');
+  this.route('userHome');
+  this.route('signUp');
+});
+
+export default Router;
