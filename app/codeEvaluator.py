902c2f1a8bc20706605b26d1452bdc9a43dde264
jithin <jithinbp@gmail.com>
2017-08-19 23:48:20 +0530

    User scripts : Made all IDs unique and enabled buttons to be linked to any target (#64)
    * filename is no longer unique. fixes #62
    
    * elements return their ID property in codeEvaluator
    
     WIP #61 . users can now store a reference to the html objects they create, and modifty them later if required
    
    * added unique names for elements created in user scripts
    
    fixes #63
    
    * added unique names for elements created in user scripts
    
    fixes #63

--- app/codeEvaluator.py
+++ app/codeEvaluator.py
@@ -11,9 +11,9 @@ from collections import OrderedDict
 class Evaluator:
 	def __init__(self,functionList):		
 		self.generatedApp=[]
-		self.widgets = 0
 		self.hasPlot=False
-
+		self.itemList=[]
+		
 		self.evalGlobals={}
 		self.functionList = functionList
 		self.evalGlobals = functionList.copy()
@@ -23,34 +23,56 @@ class Evaluator:
 		self.evalGlobals['label']=self.label
 		self.evalGlobals['plot']=self.plot
 		
+	def toUnique(self,identifier): #Make any ID string unique. returns new string.
+		suffix=0
+		if identifier in self.itemList:
+			suffix+=1
+			return self.toUnique(identifier+str(suffix))
+		self.itemList.append(identifier)
+		return identifier
+		
 	def print(self,*args):
 		'''
 		For now, the print function is being overloaded in order to capture the console output.
 		Future plans will store each line of execution as a json object. This approach will increase flexibility,
 		and outputs more than just text, such as images and widgets can be created.
 		'''
-		self.generatedApp.append({"type":"text","name":"print","value":str(args)})
+		name=self.toUnique("print")
+		self.generatedApp.append({"type":"text","name":name,"value":[str(a) for a in args]})
+		return name
 	
 
 	def printer(self,txt,name="print"):
+		name=self.toUnique(name)
 		self.generatedApp.append({"type":"span","name":name,"class":"row well","value":str(txt)})
+		return name
 
 	def label(self,txt,name="print",html_class=""):
+		name=self.toUnique(name)
 		self.generatedApp.append({"type":"label","name":name,"class":html_class,"value":str(txt)})
+		return name
 
-	def button(self,label,endpoint,displayType="display_number"):
-		self.generatedApp.append({"type":"button", "name":"button-id%d"%self.widgets,"label":label,"fetched_value":"","action":{"type":"POST","endpoint":endpoint,"success":{"datapoint":'result',"type":displayType,"target":"button-id%d-label"%self.widgets}}})
-		if displayType=="display_number":
-			self.label('',"button-id%d-label"%self.widgets)
-		self.widgets+=1
+	def button(self,label,endpoint,displayType="display_number",**kwargs):
+		name = kwargs.get("name","button-id")
+		name=self.toUnique(name)
+		
+		targetName = kwargs.get("name","button-id-label") #Create a unique name for the target.
+		targetName = self.toUnique(targetName)
+		targetName = kwargs.get('target',targetName)   #OVerride this name if specified explicitly
+		self.generatedApp.append({"type":"button", "name":name,"label":label,"fetched_value":"","action":{"type":"POST","endpoint":endpoint,"success":{"datapoint":'result',"type":displayType,"target":targetName}}})
+		if 'target' not in kwargs:  #If a target was not specified, make a label.
+			if displayType=="display_number":
+				self.label('',targetName)
+		return name
 	
 	#Plots
 	def plot(self,x,y,**kwargs):
-		self.generatedApp.append({"type":"plot","name":kwargs.get('name','myPlot'),"data":[np.array([x,y]).T.tolist()]}) #jqplot requires [x,y] pairs . not separate datasets.
+		name = kwargs.get('name','myPlot')
+		self.generatedApp.append({"type":"plot","name":name,"data":[np.array([x,y]).T.tolist()]}) #jqplot requires [x,y] pairs . not separate datasets.
+		return name
 
 	def runCode(self,code):
 		self.generatedApp=[]
-		self.widgets = 0
 		
 		submitted = compile(code.encode(), '<string>', mode='exec')
 		self.exec_scope = self.evalGlobals.copy()
