94a430a6b7b9a927508d1fe5f1378da6c782b8fc
subhahu <34541684+subhahu123@users.noreply.github.com>
2019-02-20 02:33:17 +0530

    adding install.sh ( no need of installing package manually)

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install gunicorn==19.6.0
+pip install flask >= 0.10.1
+pip install psycopg2==2.7.2
+pip install flask-sqlalchemy
+pip install SQLAlchemy>=0.8.0
+pip install numpy>=1.13
+pip install flask-cors>=3.0.0
+pip install flasgger>=0.6.6
